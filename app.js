d00f8f73f69666565914b7b98c71cc4ca5473e0d
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-19 16:16:48 +0530

    add log for all other buttons

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
       //player attack
@@ -31,6 +32,10 @@ new Vue({
     specialAttack: function() {
       var damage = this.calculateDamage(10, 20);
       this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
@@ -43,6 +48,10 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function() {
