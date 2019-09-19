b06248889ef596c0f4334fa827012e7a852f657b
SaiChandra2896 <saichandra2896@gmail.com>
2019-09-19 16:11:47 +0530

    add heal and giveup methods and print the log

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
@@ -15,11 +16,16 @@ new Vue({
       //player attack
       var damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       }
       //monster attack
       this.monsterAttacks();
+      console.log('check', this.turns[0]);
       this.checkWin();
     },
     specialAttack: function() {
@@ -31,11 +37,24 @@ new Vue({
       this.monsterAttacks();
       this.checkWin();
     },
-    heal: function() {},
-    giveUp: function() {},
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.monsterAttacks();
+    },
+    giveUp: function() {
+      this.gameIsRunning = false;
+    },
     monsterAttacks: function() {
       var damage = this.calculateDamage(5, 12);
       this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
